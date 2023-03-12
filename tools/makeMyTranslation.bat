a4f82136283b22fb9586b3593bd544c8a5404c0c
DanielPohl <pohl.daniel@freenet.de>
2023-03-12 15:27:33 +0100

    create translation

---
+++ tools/makeMyTranslation.bat
@@ -0,0 +1,76 @@
+rd /s /q "share"
+
+md share\locale\ca_ES\LC_MESSAGES
+bin\msgfmt i18n\po\ca_ES.po -o share\locale\ca_ES\LC_MESSAGES\xmoto.mo
+
+md share\locale\cs_CZ\LC_MESSAGES
+bin\msgfmt i18n\po\cs_CZ.po -o share\locale\cs_CZ\LC_MESSAGES\xmoto.mo
+
+md share\locale\da_DK\LC_MESSAGES
+bin\msgfmt i18n\po\da_DK.po -o share\locale\da_DK\LC_MESSAGES\xmoto.mo
+
+md share\locale\de\LC_MESSAGES
+bin\msgfmt i18n\po\de.po -o share\locale\de\LC_MESSAGES\xmoto.mo
+
+
+md share\locale\es\LC_MESSAGES
+bin\msgfmt i18n\po\es.po -o share\locale\es\LC_MESSAGES\xmoto.mo
+
+md share\locale\fi\LC_MESSAGES
+bin\msgfmt i18n\po\fi.po -o share\locale\fi\LC_MESSAGES\xmoto.mo
+
+md share\locale\fr\LC_MESSAGES
+bin\msgfmt i18n\po\fr.po -o share\locale\fr\LC_MESSAGES\xmoto.mo
+
+md share\locale\gl_ES\LC_MESSAGES
+bin\msgfmt i18n\po\gl_ES.po -o share\locale\gl_ES\LC_MESSAGES\xmoto.mo
+
+
+md share\locale\hu\LC_MESSAGES
+bin\msgfmt i18n\po\hu.po -o share\locale\hu\LC_MESSAGES\xmoto.mo
+
+md share\locale\it\LC_MESSAGES
+bin\msgfmt i18n\po\it.po -o share\locale\it\LC_MESSAGES\xmoto.mo
+
+md share\locale\lt\LC_MESSAGES
+bin\msgfmt i18n\po\lt.po -o share\locale\lt\LC_MESSAGES\xmoto.mo
+
+md share\locale\lv\LC_MESSAGES
+bin\msgfmt i18n\po\lv.po -o share\locale\lv\LC_MESSAGES\xmoto.mo
+
+
+md share\locale\nb_NO\LC_MESSAGES
+bin\msgfmt i18n\po\nb_NO.po -o share\locale\nb_NO\LC_MESSAGES\xmoto.mo
+
+md share\locale\nl\LC_MESSAGES
+bin\msgfmt i18n\po\nl.po -o share\locale\nl\LC_MESSAGES\xmoto.mo
+
+md share\locale\nn_NO\LC_MESSAGES
+bin\msgfmt i18n\po\nn_NO.po -o share\locale\nn_NO\LC_MESSAGES\xmoto.mo
+
+md share\locale\pl\LC_MESSAGES
+bin\msgfmt i18n\po\pl.po -o share\locale\pl\LC_MESSAGES\xmoto.mo
+
+
+md share\locale\pt\LC_MESSAGES
+bin\msgfmt i18n\po\pt.po -o share\locale\pt\LC_MESSAGES\xmoto.mo
+
+md share\locale\pt_BR\LC_MESSAGES
+bin\msgfmt i18n\po\pt_BR.po -o share\locale\pt_BR\LC_MESSAGES\xmoto.mo
+
+md share\locale\ru\LC_MESSAGES
+bin\msgfmt i18n\po\ru.po -o share\locale\ru\LC_MESSAGES\xmoto.mo
+
+md share\locale\sk\LC_MESSAGES
+bin\msgfmt i18n\po\sk.po -o share\locale\sk\LC_MESSAGES\xmoto.mo
+
+
+md share\locale\sv_SE\LC_MESSAGES
+bin\msgfmt i18n\po\sv_SE.po -o share\locale\sv_SE\LC_MESSAGES\xmoto.mo
+
+md share\locale\tr\LC_MESSAGES
+bin\msgfmt i18n\po\tr.po -o share\locale\tr\LC_MESSAGES\xmoto.mo
+
+md share\locale\zh_TW\LC_MESSAGES
+bin\msgfmt i18n\po\zh_TW.po -o share\locale\zh_TW\LC_MESSAGES\xmoto.mo
+
